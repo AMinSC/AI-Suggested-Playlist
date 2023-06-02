7fd0695bac96229c769ac1797aa04af34f09905e
AMinSC <cdtjdcjf@gmail.com>
2023-06-02 14:55:49 +0900

    ing..

---
+++ components/youtubeDataV3.js
@@ -0,0 +1,48 @@
+class Search{
+    constructor($youtube_api, $vedioList) {
+        this.YOUTUBE_API = $youtube_api
+        this.$vedioList = $vedioList;
+        this.videoIdCounter = 0;
+        this.gptRecList = ''
+        //YOUTUBE DATA API v3. Search
+        //help for params : https://developers.google.com/youtube/v3/docs/search/list#--
+        this.optionParams={
+            q: '',
+            part: "snippet",
+            key: YOUTUBE_API,
+            type: "video",
+            maxResults: 1,
+            order: "viewCount",
+            regionCode: "KR",
+            videoDuration: "medium",
+            videoEmbeddable: "true",  // ture일 경우 퍼갈 수 있는 영상만 검색
+        };
+        //한글을 검색어로 전달하기 위해선 url encoding 필요!
+        this.optionParams.q = encodeURI(optionParams.q);
+    }
+
+    // youtube api 를 활용하여, ChatGPT가 추천해준 노래 리스트를 search 한 뒤, 영상 id 가져오는 함수
+    getVideoId(recList) {
+        for (const i of recList) {
+            optionParams['q'] = i
+            let url="https://www.googleapis.com/youtube/v3/search?";
+            for(let option in optionParams){
+                url += option + "=" + optionParams[option] + "&";
+            }
+    
+            //url의마지막에 붙어있는 & 정리
+            url = url.substr(0, url.length - 1);
+    
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(`2 : ${data.items[0].id.videoId}`)
+                    // printVedio(data.items[0].id.videoId)
+                })
+                .catch(error => console.log(error));
+        }
+    }
+
+}
+
+export default Search;
--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 import Gpt from './components/chatGPT.js'
+import Search from './components/youtubeDataV3.js'
+import { config } from './apikey.js'
 
-new Gpt(
-    document.querySelector("form"),
-    document.querySelector("input"),
-    document.querySelector("ul")
-    );
+async function main() {
+    const gptInstance = new Gpt(
+        document.querySelector("form"),
+        document.querySelector("input"),
+        document.querySelector("ul")
+        );
+    await gptInstance.getAnswer()
+    await console.log(gptInstance.getAnswer())
+}
+
+
+// new Search(
+//         config.apikey,
+//         document.querySelector("#vedioList")
+//         )
+
+main()
