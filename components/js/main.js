1e1f22063883f35a5fe4df8c7827cceb6023bae0
AMinSC <cdtjdcjf@gmail.com>
2023-06-08 01:04:24 +0900

    youtube api 주석 제거

--- components/js/main.js
+++ components/js/main.js
@@ -60,15 +60,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
         console.log(answer);
         
         // 할당량 복구까지 아래 코드 주석, 로딩 체크(할당량)
-        // const InstanceSearch = new Search(answer)
-        // const videoId = await InstanceSearch.getVideoId();
-        // console.log(videoId)
+        const InstanceSearch = new Search(answer)
+        const videoId = await InstanceSearch.getVideoId();
+        console.log(videoId)
         
-        // // videoId를 순회하며 플레이어 등록하기
-        // for (const i of videoId) {
-        //     await ytReady;
-        //     const $li = document.createElement("li");
-        //     InstancePlayer.printvideo($videoList, $li, i)
-        // }
+        // videoId를 순회하며 플레이어 등록하기
+        for (const i of videoId) {
+            await ytReady;
+            const $li = document.createElement("li");
+            InstancePlayer.printvideo($videoList, $li, i)
+        }
     })
 });
