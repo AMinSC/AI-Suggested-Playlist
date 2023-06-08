7ef5d05d4e907d63e77d40df7d70f560c9207e25
AMinSC <cdtjdcjf@gmail.com>
2023-06-08 09:16:24 +0900

    youtubeDataV3.js  getVideoId 함수 수정

--- components/js/youtubeDataV3.js
+++ components/js/youtubeDataV3.js
@@ -1,13 +1,38 @@
 class Search {
     constructor() {
         this.videoIdList = []
+
+        //YOUTUBE DATA API v3. Search
+        //help for params : https://developers.google.com/youtube/v3/docs/search/list#--
+        this.optionParams={
+            q: '',
+            part: "snippet",
+            key: this.YOUTUBE_API,
+            type: "video",
+            maxResults: 1,
+            order: "viewCount",
+            regionCode: "KR",
+            videoDuration: "medium",
+            videoEmbeddable: "true",  // ture일 경우 퍼갈 수 있는 영상만 검색
         }
+
+        //한글을 검색어로 전달하기 위해선 url encoding 필요!
+        this.optionParams.q = encodeURI(this.optionParams.q);
+    }
     
-        // Function that retrieves the video id after searching the list of songs recommended by ChatGPT
-        async getVideoId(videoList) {
+    // Function that retrieves the video id after searching the list of songs recommended by ChatGPT
+    async getVideoId(videoList) {
+        this.videoIdList = []
         for (const i of videoList) {
-            const url = `/.netlify/functions/youtube?q=${i}`;
-    
+            this.optionParams['q'] = i
+            let url="https://www.googleapis.com/youtube/v3/search?";
+            for(let option in this.optionParams){
+                url += option + "=" + this.optionParams[option] + "&";
+            }
+
+            //url의마지막에 붙어있는 & 정리
+            url = url.substr(0, url.length - 1);
+
             try {
             let response = await fetch(url);
             let data = await response.json();
