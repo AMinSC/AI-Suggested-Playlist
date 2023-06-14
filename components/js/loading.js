ae2afa9ad08c9e4f30ed795b46012296d5eaa27c
AMinSC <cdtjdcjf@gmail.com>
2023-06-14 17:30:29 +0900

    loading.js 파일을 css와 js에 맞게 코드 수정

--- components/js/loading.js
+++ components/js/loading.js
@@ -1,52 +1,27 @@
-/**
- * chatGPT에게 질문하고, 답변 받기까지의 시간동안 로딩을 노출하는 함수
- * @param {*} gif - 로딩화면에 노출된 gif
- */
+/* loading.js */
 export function LoadingWithMask(gif) {
-    //화면의 높이와 너비를 구합니다.
     let maskHeight = document.body.scrollHeight;
-    let maskWidth  = window.innerWidth;
+    let maskWidth = window.innerWidth;
 
-    //화면에 출력할 마스크를 설정해줍니다.
-    let mask       = document.createElement("div");
+    let mask = document.createElement("div");
     mask.id = 'mask';
-    mask.style.position = 'absolute';
-    mask.style.zIndex = '9000';
-    mask.style.backgroundColor = '#35363a';
-    mask.style.display = 'none';
-    mask.style.left = '0';
-    mask.style.top = '0';
 
     let loadingImg = document.createElement("img");
     loadingImg.src = gif;
-    loadingImg.style.backgroundColor = '#35363a';
-    loadingImg.style.position = 'absolute';
-    loadingImg.style.display = 'block';
-    loadingImg.style.margin = '0 auto';
-    loadingImg.style.left = '50%';
-    loadingImg.style.top = '50%';
-    loadingImg.style.transform = 'translate(-50%, -50%)';
+    loadingImg.className = "loading-img";
 
-    //화면에 레이어 추가
     document.body.appendChild(mask);
 
-    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
     mask.style.width = maskWidth + 'px';
     mask.style.height = maskHeight + 'px';
-    mask.style.opacity = '0.3';
 
-    //마스크 표시
-    mask.style.display = 'block';
-
-    //로딩중 이미지 표시
     mask.appendChild(loadingImg);
-    loadingImg.style.display = 'block';
 }
 
 export function closeLoadingWithMask() {
     let mask = document.getElementById('mask');
     if (mask) {
+        mask.innerHTML = '';
         mask.style.display = 'none';
-        mask.innerHTML = '';  
     }
 }
