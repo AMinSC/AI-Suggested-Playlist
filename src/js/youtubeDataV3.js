a61cb95d5ffe00ba26532d58e634c6cf9b18874e
AMinSC <cdtjdcjf@gmail.com>
2023-07-26 09:38:50 +0900

    data check4

--- src/js/youtubeDataV3.js
+++ src/js/youtubeDataV3.js
@@ -35,9 +35,12 @@ class Search {
 
             //url의마지막에 붙어있는 & 정리
             url = url.substr(0, url.length - 1);
+            console.log('url', url)
             try {
                 let response = await fetch(url);
+                console.log('response', response)
                 let data = await response.json();
+                console.log('data', data)
                 this.videoIdList.push(data.items[0].id.videoId);
             } catch (error) {
                 console.log(error);
